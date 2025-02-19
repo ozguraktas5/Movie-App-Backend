b1b3591134e5d07563476c63fd20083e5389a94f
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2025-02-19 17:41:42 +0000

    post endpoint'i basariyla test edildi, get endpoint'inde sorun var

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ MongoClient.connect(
   process.exit(1);
 })
   .then(async client => {
+    await ReviewsDAO.injectDB(client);
     app.listen(port, () => {
       console.log(`listening on port ${port}`);
     })
